e73ceab980434f5284ff33e7da7f2f17df3a0287
HisperianHub <Mario.biz.gon@gmail.com>
2018-04-08 21:03:55 +0200

    Merge branch 'MarioBG' into origin/master

--- L10 Workspace/Acme-Newspaper/src/main/java/controllers/ChirpController.java
+++ L10 Workspace/Acme-Newspaper/src/main/java/controllers/ChirpController.java
@@ -3,12 +3,9 @@ package controllers;
 
 import java.util.Collection;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,19 +62,19 @@ public class ChirpController extends AbstractController {
 		return result;
 	}
 
-	@RequestMapping(value = "/user/display", method = RequestMethod.GET)
-	public ModelAndView listUser(@RequestParam final int chirpId) {
-		ModelAndView result;
-		Chirp chirp;
-
-		chirp = this.chirpService.findOne(chirpId);
-
-		result = new ModelAndView("chirp/display");
-		result.addObject("chirp", chirp);
-		result.addObject("requestURI", "chirp/display.do");
-
-		return result;
-	}
+	//	@RequestMapping(value = "/user/display", method = RequestMethod.GET)
+	//	public ModelAndView listUser(@RequestParam final int chirpId) {
+	//		ModelAndView result;
+	//		Chirp chirp;
+	//
+	//		chirp = this.chirpService.findOne(chirpId);
+	//
+	//		result = new ModelAndView("chirp/display");
+	//		result.addObject("chirp", chirp);
+	//		result.addObject("requestURI", "chirp/display.do");
+	//
+	//		return result;
+	//	}
 
 	// Editing ---------------------------------------------------------------
 
@@ -108,81 +105,4 @@ public class ChirpController extends AbstractController {
 	//		return res;
 	//	}
 
-	// Creating ----------------------------------------------------------
-
-	@RequestMapping(value = "/user/create", method = RequestMethod.GET)
-	public ModelAndView create() {
-		ModelAndView result;
-		Chirp chirp;
-
-		chirp = this.chirpService.create();
-		result = this.createEditModelAndView(chirp);
-
-		return result;
-	}
-
-	@RequestMapping(value = "/user/create", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid final Chirp chirp, final BindingResult binding) {
-		ModelAndView res;
-
-		if (binding.hasErrors())
-			res = this.createEditModelAndView(chirp, "user.params.error");
-		else
-			try {
-				this.chirpService.save(chirp);
-				res = new ModelAndView("redirect:/chirp/list.do");
-			} catch (final Throwable oops) {
-				res = this.createEditModelAndView(chirp, "user.commit.error");
-			}
-
-		return res;
-	}
-
-	//Deleting ---------------------------------
-
-	@RequestMapping(value = "/admin/delete", method = RequestMethod.GET)
-	public ModelAndView delete(final int chirpId) {
-
-		Assert.notNull(this.adminService.findByPrincipal());
-
-		final Chirp chirp = this.chirpService.findOne(chirpId);
-
-		this.chirpService.delete(chirp);
-
-		return new ModelAndView("redirect:/");
-	}
-
-	@RequestMapping(value = "/admin/edit", method = RequestMethod.POST, params = "delete")
-	public ModelAndView delete(final Chirp chirp, final BindingResult binding) {
-		ModelAndView result;
-
-		try {
-			this.chirpService.delete(chirp);
-			result = new ModelAndView("redirect:/");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(chirp, "application.commit.error");
-		}
-
-		return result;
-	}
-
-	//Ancillary methods ------------------------
-
-	protected ModelAndView createEditModelAndView(final Chirp chirp) {
-		ModelAndView result;
-
-		result = this.createEditModelAndView(chirp, null);
-
-		return result;
-	}
-
-	protected ModelAndView createEditModelAndView(final Chirp chirp, final String message) {
-		ModelAndView result;
-
-		result = new ModelAndView("chirp/create");
-		result.addObject("chirp", chirp);
-		result.addObject("message", message);
-
-		return result;
-	}
 }
