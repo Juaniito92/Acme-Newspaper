ed9e7b060ea5bc3679fa97e52ca5a019ef55abf9
marmargal <inma.martin.galvan@hotmail.com>
2018-04-08 19:36:45 +0200

    List de Articles de User

--- L10 Workspace/Acme-Newspaper/src/main/java/repositories/ArticleRepository.java
+++ L10 Workspace/Acme-Newspaper/src/main/java/repositories/ArticleRepository.java
@@ -1,11 +1,17 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+
+	@Query("select a from Article a where(a.title LIKE %?1% or a.summary LIKE %?1% or a.body LIKE %?1%) and a.publicationMoment <= current_date")
+	Collection<Article> findPerKeyword(String aux);
 
 }
